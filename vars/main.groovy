0fd9e1a122b301dbd1a52cf53ab194474bb4cc5b
Somya Gaur <39803705+Somyagaur06@users.noreply.github.com>
2020-09-26 14:07:05 +0530

    Update main.groovy

--- vars/main.groovy
+++ vars/main.groovy
@@ -1,1 +1,68 @@
 
+def call(){
+
+
+node{
+ 
+  
+
+
+ stage("Git Checkout")
+ {
+ 
+   git 'https://github.com/Somyagaur06/jenkinsDemo.git'
+ 
+ 
+ }
+
+
+
+stage("Maven Build"){
+
+ dir("/var/lib/jenkins/workspace/sharedLibrary2/jenkinsDemo"){
+
+docker.image('maven:3.3.3-jdk-8').inside{
+ 
+ sh "mvn clean package"
+
+}}}
+
+
+stage("Tomcat Deployment"){
+
+
+sshagent(['deploy_user']){
+          
+  sh " scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/sharedLibrary2/jenkinsDemo/target/*.war ubuntu@172.31.1.78:/opt/apache-tomcat/webapps "
+
+          
+}}
+          
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}}
+
